ec666f67f1a8aa13b9b5adc5d1c2c909c69c4f9b
hspark1124 <hspark@woongjin.co.kr>
2025-05-30 00:38:32 +0000

    20250530-0

--- frontend/report_page.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from utils import get_recommendations
-
-def report_page():
-    pass
--- frontend/simulation_page.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from utils import get_recommendations
-
-def simulation_page():
-    pass
